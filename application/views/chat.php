24dc95d9659ceeb1404f57776b9dfcb86213e2c1
David Rodal <dave.rodal@gmail.com>
2011-11-08 22:19:58 -0500

    added a comment

--- application/views/chat.php
+++ application/views/chat.php
@@ -9,6 +9,7 @@
     #city {position:absolute;top:300px;width:100px;height:100px;background:#ccf;}
   </style>
   <script>
+/* a comment */
   $(document).ready(function() {
 	  $("#draggable").draggable({stop:
 	  function(event,ui){
