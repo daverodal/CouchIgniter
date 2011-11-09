f5693d678e845a753ff5771323c227d0d1317458
David Rodal <dave.rodal@gmail.com>
2011-11-08 20:39:18 -0500

    Testing phpstorm and github

--- application/controllers/lobby.php
+++ application/controllers/lobby.php
@@ -8,7 +8,7 @@ class Lobby extends CI_Controller
         if (!$user) {
             redirect("/lobby/login/");
         }
-        echo "Welcome $user";
+        echo "Welcoeme $user";
         $this->load->view("lobby/lobbyView");
 
     }
