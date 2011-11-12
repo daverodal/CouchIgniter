a918ded9d51948ffe5c5327bdc9e590083b6e3e1
David Rodal <dave.rodal@gmail.com>
2011-11-11 19:41:36 -0500

    minor cleanup

--- application/views/lobby/lobbyView.php
+++ application/views/lobby/lobbyView.php
@@ -1,9 +1,8 @@
 <!doctype html>
 <html>
     <?=$this->load->view("lobby/lobbyHead.php");?>
-
     <body>
-        <a href="<?=site_url("lobby/logout");?>"/>logout</a>
+        <a href="<?=site_url("lobby/logout");?>">logout</a>
         <form onsubmit="doit();return false;" id="chatform" method="post">
             <fieldset style="float:left;">
                 <legend>Time
@@ -28,8 +27,6 @@
                 </legend>
                 <div id="games"></div>
             </fieldset>
-
-
         </form>
         <div style="height:100px;width:100px;background:pink;position:relative">
             <div style="position:absolute;top:14px;left:20px;height:16px;width:16px;border-radius:8px;background:black;"></div>
@@ -40,3 +37,4 @@
             <div style="position:absolute;top:64px;right:20px;height:16px;width:16px;border-radius:8px;background:black;"></div>
         </div>
     </body>
+</html>
