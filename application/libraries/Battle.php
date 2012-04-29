5eda1bef7e5d98ed7adf76c73e6f05be4de0ff6c
Dave Rodal <dave.rodal@gmail.com>
2012-04-29 12:33:15 -0400

    made server dev env changes easier

--- application/libraries/Battle.php
+++ application/libraries/Battle.php
@@ -5,49 +5,49 @@
  * Time: 5:03 PM
  * Link: http://davidrodal.com
  * */
+define ("WARGAMES","/Documents and Settings/Owner/Desktop/");
 class Battle
 {
 
     public static  function getBattle($name,$doc){
         switch($name){
             case "MartianCivilWar":
-                @include_once("/home/davidrod/webwargaming/MartianCivilWar.php");
-                //@include_once("/Documents and Settings/Owner/Desktop/webwargaming/MartianCivilWar.php");
+//                @include_once("/home/davidrod/webwargaming/MartianCivilWar.php");
+                @include_once(WARGAMES."MCW/MartianCivilWar.php");
                 break;
             case "NapOnMars":
-                @include_once("/Documents and Settings/Owner/Desktop/NapOnMars/NapOnMars.php");
+                @include_once(WARGAMES."NapOnMars/NapOnMars.php");
                 break;
             case "BattleOfMoscow":
-                @include_once("/Documents and Settings/Owner/Desktop/webwargaming/BattleOfMoscow.php");
+                @include_once(WARGAMES."webwargaming/BattleOfMoscow.php");
                 break;
             case "BattleForAllenCreek":
-                @include_once("/Documents and Settings/Owner/Desktop/BfAC/BattleForAllenCreek.php");
+                @include_once(WARGAMES."BfAC/BattleForAllenCreek.php");
                 break;
             default:
                 throw(new Exception("Bad Class Dude!"));
         }
         return new $name($doc);
     }
-
     public static function getView($name){
         switch($name){
             case "MartianCivilWar":
-                @include_once("/Documents and Settings/Owner/Desktop/MCW/MartianCivilWar.php");
+                @include_once(WARGAMES."MCW/MartianCivilWar.php");
                 MartianCivilWar::getView();
-//                @include_once("/Documents and Settings/Owner/Desktop/webwargaming/view.php");
+//                @include_once(WARGAMES."webwargaming/view.php");
                 break;
             case "NapOnMars":
-                @include_once("/Documents and Settings/Owner/Desktop/NapOnMars/napOnMars.php");
+                @include_once(WARGAMES."NapOnMars/napOnMars.php");
                 NapOnMars::getView();
 //                @include_once("/Documents and Settings/Owner/Desktop/webwargaming/view.php");
                 break;
            case "BattleOfMoscow":
-               @include_once("/Documents and Settings/Owner/Desktop/webwargaming/BattleOfMoscow.php");
-                @include_once("/Documents and Settings/Owner/Desktop/webwargaming/view.php");
+               @include_once(WARGAMES."webwargaming/BattleOfMoscow.php");
+                @include_once(WARGAMES."webwargaming/view.php");
                 break;
             case "BattleForAllenCreek":
-                @include_once("/Documents and Settings/Owner/Desktop/BfAC/BattleForAllenCreek.php");
-                @include_once("/Documents and Settings/Owner/Desktop/BfAC/view.php");
+                @include_once(WARGAMES."BfAC/BattleForAllenCreek.php");
+                @include_once(WARGAMES."BfAC/view.php");
                 break;
             default:
                 throw(new Exception("Bad Class Dude!"));
@@ -56,22 +56,22 @@ class Battle
     public static function getHeader($name,$data){
         switch($name){
             case "MartianCivilWar":
-                @include_once("/Documents and Settings/Owner/Desktop/MCW/MartianCivilWar.php");
+                @include_once(WARGAMES."MCW/MartianCivilWar.php");
 //                @include_once("/Documents and Settings/Owner/Desktop/webwargaming/header.php");
                 MartianCivilWar::getHeader($data);
              break;
             case "NapOnMars":
-                @include_once("/Documents and Settings/Owner/Desktop/NapOnMars/NapOnMars.php");
+                @include_once(WARGAMES."NapOnMars/NapOnMars.php");
 //                @include_once("/Documents and Settings/Owner/Desktop/webwargaming/header.php");
                 NapOnMars::getHeader($data);
                 break;
             case "BattleOfMoscow":
-                @include_once("/Documents and Settings/Owner/Desktop/webwargaming/BattleOfMoscow.php");
-                @include_once("/Documents and Settings/Owner/Desktop/webwargaming/header.php");
+                @include_once(WARGAMES."webwargaming/BattleOfMoscow.php");
+                @include_once(WARGAMES."webwargaming/header.php");
                 break;
             case "BattleForAllenCreek":
-                @include_once("/Documents and Settings/Owner/Desktop/BfAC/BattleForAllenCreek.php");
-                @include_once("/Documents and Settings/Owner/Desktop/BfAC/header.php");
+                @include_once(WARGAMES."BfAC/BattleForAllenCreek.php");
+                @include_once(WARGAMES."BfAC/header.php");
                 break;
             default:
                 throw(new Exception("Bad Class Dude!"));
@@ -82,16 +82,16 @@ class Battle
     public static function loadGame($name){
         switch($name){
             case "MartianCivilWar":
-                @include_once("/Documents and Settings/Owner/Desktop/MCW/MartianCivilWar.php");
+                @include_once(WARGAMES."MCW/MartianCivilWar.php");
                 break;
             case "NapOnMars":
-                @include_once("/Documents and Settings/Owner/Desktop/NapOnMars/NapOnMars.php");
+                @include_once(WARGAMES."NapOnMars/NapOnMars.php");
                 break;
             case "BattleOfMoscow":
-                @include_once("/Documents and Settings/Owner/Desktop/webwargaming/BattleOfMoscow.php");
+                @include_once(WARGAMES."webwargaming/BattleOfMoscow.php");
                 break;
             case "BattleForAllenCreek":
-                @include_once("/Documents and Settings/Owner/Desktop/BfAC/BattleForAllenCreek.php");
+                @include_once(WARGAMES."BfAC/BattleForAllenCreek.php");
                 break;
             default:
                 throw(new Exception("Bad Class Dude!"));
