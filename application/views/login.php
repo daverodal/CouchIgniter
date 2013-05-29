af95579288c984e125b54210f82914b7bd9867f1
David Rodal <dave.rodal@gmail.com>
2013-05-28 22:34:58 -0400

    Made login page nicer

--- application/views/login.php
+++ application/views/login.php
@@ -1,6 +1,40 @@
+<!doctype html>
+<html>
+<head>
+<style type="text/css">
+    body{
+        background: url('<?=base_url("js/civil-war-public-domain-horse.jpg");?>');
+        background-repeat: no-repeat;
+        background-size:    100%;
+
+    }
+    div{
+        font-size:22px;
+        background:rgba(255,255,255,.9);
+        border:1px solid #333;
+        border-radius:15px;
+        margin:40px;
+        padding:20px;
+        float:left;
+    }
+    input{
+        margin-left:15px;
+    }
+    a{
+        color:#333;
+    }
+</style>
+</head>
+<body>
+<div>
+    Please login:
 <form method="POST">
-Your name?<input type="text" name="name"><br>
+Username<input type="text" name="name"><br>
     Password
     <input type="password" name="password">
     <input type="submit">
 </form>
+    <a href="/">Or back to front page</a>
+</div>
+</body>
+</html>
