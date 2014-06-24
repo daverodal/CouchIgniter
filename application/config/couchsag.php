34405532e65e36ae90aeb0dc6a1b8244b56c5664
David M. Rodal <dave.rodal@gmail.com>
2014-06-24 11:49:06 -0500

    made couchdb include private.php for password protectiojn

--- application/config/couchsag.php
+++ application/config/couchsag.php
@@ -1,5 +1,6 @@
-<?php
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config["host"] = "127.0.0.1";
 $config["port"] = 5984;
 $config['database'] = 'mydatabase';
+include "private.php";
