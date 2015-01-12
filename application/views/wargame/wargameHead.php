9ec308925934f44d9b9f3b6f01ae87ac318c3581
daverodal <dave.rodal@gmail.com>
2015-01-11 19:04:16 -0500

    jquery mobile

--- application/views/wargame/wargameHead.php
+++ application/views/wargame/wargameHead.php
@@ -6,6 +6,7 @@
 <!--    <script src="--><?//=base_url("js/jquery.ui.touch-punch.min.js");?><!--"></script>-->
     <script src="<?=base_url("js/jquery.panzoom/dist/jquery.panzoom.js");?>"></script>
     <script src="<?=base_url("js/jquery.panzoom/test/libs/jquery.mousewheel.js");?>"></script>
+    <script src="//code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
     <script src="<?=base_url("js/sync.js");?>"></script>
     <?php Battle::getHeader($gameName, $playerData, $arg);?>
 </head>
