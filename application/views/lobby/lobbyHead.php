4c1691ca49974a02d8be106110f83ebe6ba313be
daverodal <dave.rodal@gmail.com>
2015-03-08 18:53:01 -0400

    License stuff

--- application/views/lobby/lobbyHead.php
+++ application/views/lobby/lobbyHead.php
@@ -1,4 +1,22 @@
-<head>
+<?php
+/**
+ *
+ * Copyright 2011-2015 David Rodal
+ *
+ *  This program is free software; you can redistribute it
+ *  and/or modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation;
+ *  either version 2 of the License, or (at your option) any later version
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?><head>
     <link rel="shortcut icon" href="/favicon.ico" type="image/icon">
     <link href="<?=base_url("js/jquery-ui-1.11.0.css");?>" rel="stylesheet" type="text/css"/>
     <script src="<?=base_url("js/jquery-1.11.1.min.js");?>"></script>
