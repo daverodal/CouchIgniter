3b43722bc41c1030e9aa06c95b9b9d3e2c0c058c
daverodal <dave.rodal@gmail.com>
2015-03-08 21:38:46 -0400

    License stuff

--- application/views/wargame/wargameLobbyView.php
+++ application/views/wargame/wargameLobbyView.php
@@ -56,6 +56,7 @@
     <my-pane title="Welcome">
         <div class="coolBox">
           <h1>Welcome to davidrodal.com.</h1>
+            <h2> New licence info.</h2>
             <h2>There is a NEW scenario for "Chawinda 1965" available.</h2>
 
             <h2><a href="<?= site_url("wargame/unattachedGame/TMCW/20%27th%20Century%20Games/Chawinda1965"); ?>">Browse to it here if you like.</a></h2>
